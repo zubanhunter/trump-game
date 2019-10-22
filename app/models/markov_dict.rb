c3e1c480673a0a8a3f1eb90d5fea3920955d9dd0
Alexander Woods <alexwoo@amazon.com>
2019-10-22 09:44:52 -0700

    Initial version.

---
+++ app/models/markov_dict.rb
@@ -0,0 +1,83 @@
+class MarkovDict
+  attr_reader :order
+
+  # Initialized the dictionary with a text source.
+  #
+  # @example Create a new dictionary of order 1
+  #   MarkovChains::Dictionary.new(string)
+  # @example Create a new dictinary of order 2
+  #   MarkovChains::Dictionary.new(string, 2)
+  #
+  # @param [String] the text source
+  # @param [int] the order of the dictionary. The order is the "memory" of the dictionary, meaning that an order <n> dictionary will consider <n> words to generate the next one. Order of 1 or 2 are typical. More than 3 and the generated sentences will be the same as the source.
+  #
+  def initialize(order: 1)
+    @order = order
+    @words_for = Hash.new
+    @start_words = Array.new
+  end
+
+  def process_text(text)
+    cleaned_text = clean_text(text)
+    seps = /([.!?]+)/
+    sentences = text.split seps
+    sentences.each_slice(2) { |s,t| process_sentence(s.strip,t) }
+  end
+
+  # Processes a single sentence with terminator
+  #
+  # @example Process a sentence
+  #   process_sentence("It is sunny today", "!")
+  #
+  # @param [String] sentence to process
+  # @param [Character] sentence terminator
+  #
+  def process_sentence(sentence, terminator)
+    # Consider phrases/words/clauses separators when splitting
+    seps = "([,;:])"
+
+    # Split <sentence> into words
+    words = sentence.gsub(/[^#{seps}\w'\s]/, "").gsub(/(#{seps})\s+/, '\1').split(/\s+|#{seps}/)
+    return if words.size <= order
+    words << terminator
+
+    # Add <@order> start words to the list
+    @start_words << words[0, @order]
+
+    # Add the words to the frequency hash <words_for>
+    until words.size < @order + 1 do
+      (@words_for[words[0, @order]] ||= []) << words[@order]
+      words.shift
+    end
+  end
+
+  # Returns a word based on the likelihood of it appearing after the input array of words
+  #
+  # @example Get a word likely to appear next to the word 'It'
+  #   get(['It'])           # => 'has'
+  # @example Get a word likely to appear next to the words 'It has been' (with a dictionary of order 2)
+  #   get(['It has'])  # => 'been'
+  #
+  # @param [[String]] array of words for which we want a possible next word
+  # @return [String] word that is likely to follow the input
+  #
+  def get(words)
+    (@words_for[words] || []).sample
+  end
+
+  # Returns a list of words beginning a sentence seen in the source
+  #
+  # @example Get a start words
+  #   get_start_word    # => ['It', 'has']
+  #
+  # @return [[String]] array of words that could start a sentence
+  #
+  def get_start_words
+    @start_words.sample
+  end
+
+  # Clean text to remove shit
+  def clean_text(text)
+    text # TODO
+  end
+end
---
+++ app/models/markov_gen.rb
@@ -0,0 +1,37 @@
+class MarkovGen
+
+    # Initializes the generator
+    #
+    # @example Create a new generator
+    #   MarkovChains::Generator.new(text)
+    #
+    # @param Text source to generate sentences from
+    #
+    def initialize(markovDict)
+      @dict = markovDict
+    end
+
+    # Returns a given number of randonly generated sentences
+    #
+    # @example Get 5 sentences
+    #   get_sentences(5)
+    #
+    # @param n [int] number of sentences to generate
+    # @return Array conataining generated sentences
+    #
+    def get_sentences(n)
+      sentences = []
+
+      n.times do
+        sentence = @dict.get_start_words
+
+        while nw = @dict.get(sentence[-@dict.order, @dict.order])
+          sentence << nw
+        end
+
+        sentences << (sentence[0...-1].join(" ").gsub(/\s([,;:])/, '\1') << sentence.last)
+      end
+
+      sentences
+    end
+  end
